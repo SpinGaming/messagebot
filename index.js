10ce8c8d882317708da5f468e6af35fe09fd854e
SpinGaming <46069683+SpinGaming@users.noreply.github.com>
2019-12-15 12:35:19 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,56 +1,10 @@
 const Discord = require('discord.js');
-const bot = new Discord.Client();
-const token = 'NjU1NDMwMTExODc2ODc0Mjcx.XfT-7A.H4QGBDzLKZpfk6tqeraLDwLvb6Q';
-const ytdl = require("ytdl-core");
-
-var PREFIX = '|';
-var version = '1.0.1';
-var servers = {};
-
+const bot = new Client();
+const token = "NjU1NzQ2MjU4NTIyMDc5MjMy.XfYljw.qHNIFMRzVv6bTS8rECJa90fFvp8"
+const PREFIX = "$";
 
 bot.on('ready', () => {
-    console.log('This Bot Is Online!');
-})
-
-bot.on('message', message => {
-
-    let args = message.content.substring(PREFIX.length).split(" ");
-
-    switch (args[0]) {
-        case 'ping':
-            message.channel.sendMessage('Pong!')
-            break;
-
-        case 'clear':
-            if (!args[1]) return message.reply('Error please define the command!')
-            message.channel.bulkDelete(args[1]);
-            break;
-
-        case 'info':
-
-            const embed = new Discord.RichEmbed()
-                .setTitle("Josh's Slave")
-                .setColor(0x7289DA)
-                .addField('Prefix:', PREFIX)
-                .addField('Version:', version)
-                .addField('Employed In:', message.guild.name, true)
-                .addField('Developer:', '@Joshh#0922', true)
-                .setThumbnail("https://cdn-images-1.medium.com/max/1600/1*n4-mfREc-TgooqGwu2YHnw.png")
-                .setFooter('ID: '+ bot.user.id +" | Josh's Slave since:")
-                .setTimestamp(bot.user.createdAt);
-            message.channel.sendEmbed(embed);
-            break;
-
-        case 'prefix':
-            if (!args[1]) { return message.reply('You need to tell me what to listen for!') } else {
-                PREFIX = args[1];
-                message.channel.sendMessage('The prefix is now: ' + PREFIX);
-            };
-            break;
-        
-    }
-})
-
-
+    console.log("The bot is online!");
+});
 
 bot.login(process.env.BOT_TOKEN);
