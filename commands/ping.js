19754f7c569c79a2b0c52a92ad753368bb917bbd
SpinGaming <46069683+SpinGaming@users.noreply.github.com>
2019-12-15 16:53:24 +0000

    Add files via upload

--- commands/ping.js
+++ commands/ping.js
@@ -2,8 +2,7 @@ module.exports = {
     name: 'ping',
     description: 'Says pong!',
     execute(message, args){
-        const apiPing = Math.round(message.bot.ping); // This will round the api ping of the client
 
-        message.channel.send("**" + "My ping is: " + apiPing + "ms" + "**");
+        message.channel.send("**" + "My ping is: " + Math.round(message.bot.ping) + "ms" + "**");
     }
 }
