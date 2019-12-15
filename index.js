ffbe06162a27e63f6423ec7e2de3a3315f3c79c7
SpinGaming <46069683+SpinGaming@users.noreply.github.com>
2019-12-15 16:12:25 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,26 +1,11 @@
 const Discord = require('discord.js');
-const bot = new Discord.Client();
+const client = new Discord.Client();
 
-var PREFIX = '$';
-var version = '1.0.1';
-
-
-bot.on('ready', () => {
-    console.log('This Bot Is Online!');
-});
-
-bot.on('message', message => {
-
-    let args = message.content.substring(PREFIX.length).split(" ");
-
-    switch (args[0]) {
-        case 'ping':
-            message.channel.sendMessage('Pong!')
-            break;
-        
-    }
+client.on('ready', () => {
+    console.log('I am ready!');
 });
 
 
 
-bot.login(token);
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
