e1042f4b1b1cf2beb9a3761f233c968be32ded26
SpinGaming <46069683+SpinGaming@users.noreply.github.com>
2019-12-15 17:06:17 +0000

    Add files via upload

--- commands/ping.js
+++ commands/ping.js
@@ -2,12 +2,12 @@ module.exports = {
     name: 'ping',
     description: 'Says pong!',
     execute(message, args){
-            let PingEmbed = new RichEmbed()
+            const PingEmbed = new RichEmbed()
                 .setTitle("🏓 Pong!")
                 .setColor(0x00FF00)
                 .setDescription("Ping: ")
                 .setFooter("Message Bot Created By Joshh#0922")
                 .setTimestamp(Date.now());
-        message.channel.send(PingEmbed);
+        message.channel.sendEmbed(PingEmbed);
     }
 }
