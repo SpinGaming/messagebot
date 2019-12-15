2a95d0929d3a6dea9dfb0bdbe9db2631ecc67c99
SpinGaming <46069683+SpinGaming@users.noreply.github.com>
2019-12-15 19:26:15 +0000

    Add files via upload

--- commands/ping.js
+++ commands/ping.js
@@ -4,7 +4,7 @@ module.exports = {
     execute(message, args){
         const embed = new Discord.RichEmbed()
                 .setTitle("🏓 Pong!")
-                .setColor(0x7289DA)
+                .setColor("e64e4e")
                 .setDescription("Ping")
                 .setThumbnail("http://icons.iconarchive.com/icons/martz90/hex/512/messages-icon.png")
                 .setFooter("ID: ${bot.user.id}  | Message Bot By <@366614368923942912>")
