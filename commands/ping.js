8ff370fb6d85fdebf02b843c71487b9772bb49f3
SpinGaming <46069683+SpinGaming@users.noreply.github.com>
2019-12-15 16:39:15 +0000

    Add files via upload

--- commands/ping.js
+++ commands/ping.js
@@ -2,6 +2,15 @@ module.exports = {
     name: 'ping',
     description: 'Says pong!',
     execute(message, args){
-        message.channel.send('Pong!');
+        const apiPing = Math.round(message.client.ping); // This will round the api ping of the client
+        const responseTime = Math.round(Date.now() - message.createdTimestamp); // This will round the response time between when the message was received and when the message was sent
+
+        let PongEmbed = new Discord.RichEmbed()
+            .setTitle("🏓" + "**" + "Pong!" + "**")
+            .setDescription("Ping: " + responseTime + "ms")
+            .setTimestamp(Date.now())
+            .setFooter("Message Bot Created By Joshh#0922");
+
+        message.channel.sendEmbed(PongEmbed);
     }
 }
