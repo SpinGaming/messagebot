3b5698d80ba59caf9d6b32895e8b0a8f12532e8f
SpinGaming <46069683+SpinGaming@users.noreply.github.com>
2019-12-15 20:08:52 +0000

    Add files via upload

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -6,10 +6,13 @@ module.exports = {
     execute(message, args){
 
         let embed = new Discord.RichEmbed()
+            .setTitle("Message Bot")
+            .setThumbnail("http://icons.iconarchive.com/icons/martz90/hex/512/messages-icon.png")
             .setColor(0x00FF00)
             .addField('Version', version, true)
             .addField('Library', 'index.js',true)
             .addField('Developer', '<@366614368923942912>')
+            .setFooter("Message bot created by <@366614368923942912>")
             
         message.channel.send({ embed: embed });
     }
