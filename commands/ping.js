3000daaad71921747912551f971205a3aae26d9c
SpinGaming <46069683+SpinGaming@users.noreply.github.com>
2019-12-15 16:50:19 +0000

    Add files via upload

--- commands/ping.js
+++ commands/ping.js
@@ -3,7 +3,6 @@ module.exports = {
     description: 'Says pong!',
     execute(message, args){
         const apiPing = Math.round(message.bot.ping); // This will round the api ping of the client
-        const responseTime = Math.round(Date.now() - message.createdTimestamp); // This will round the response time between when the message was received and when the message was sent
 
         message.channel.send("**" + "My ping is: " + apiPing + "ms" + "**");
     }
