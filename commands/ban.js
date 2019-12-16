4c1d1fd9df949d7a66fb21c7bce3c66f2020e6bf
SpinGaming <46069683+SpinGaming@users.noreply.github.com>
2019-12-16 22:16:05 +0000

    Add files via upload

--- commands/ban.js
+++ commands/ban.js
@@ -4,46 +4,46 @@ var version = '1.0.1';
 module.exports = {
     name: 'ban',
     description: 'Bans a person',
-    execute(message, args){
+    execute(message, args) {
 
-if(message.author.hasPermissions(['BAN_MEMBERS'])){
+        if (message.member.roles.some(r => ["MessageBot"].includes(r.name))) {
 
-        const user = message.mentions.users.first();
+            const user = message.mentions.users.first();
 
-        if (user) {
-            const member = message.guild.member(user);
+            if (user) {
+                const member = message.guild.member(user);
 
-            if (member) {
-                member.ban(`Banned by: ${message.author.tag}`).then(() => {
-                    const BanSuccess = new Discord.RichEmbed()
-                    .setTitle("✅ Successful Ban")
-                    .setDescription(`Successfully banned ${user.tag}`)
-                    .setColor(0x00FF00)
-                    .setTimestamp();
-                    message.channel.send({ embed: BanSuccess});
-message.delete();
+                if (member) {
+                    member.ban(`Banned by: ${message.author.tag}`).then(() => {
+                        const BanSuccess = new Discord.RichEmbed()
+                            .setTitle("✅ Successful Ban")
+                            .setDescription(`Successfully banned ${user.tag}`)
+                            .setColor(0x00FF00)
+                            .setTimestamp();
+                        message.channel.send({ embed: BanSuccess });
+                        message.delete();
 
-                }).catch(err => {
-                    const BanFailed = new Discord.RichEmbed()
-                    .setTitle("❌ Failed Ban")
-                    .setDescription(`Unable to ban ${user.tag}`)
-                    .setColor('RED')
-                    .setTimestamp();
-                    message.channel.send({ embed: BanFailed});
-message.delete();
-                    console.log(err);
-                });
+                    }).catch(err => {
+                        const BanFailed = new Discord.RichEmbed()
+                            .setTitle("❌ Failed Ban")
+                            .setDescription(`Unable to ban ${user.tag}`)
+                            .setColor('RED')
+                            .setTimestamp();
+                        message.channel.send({ embed: BanFailed });
+                        message.delete();
+                        console.log(err);
+                    });
+                } else {
+                    message.reply("Must specify a person");
+                    message.delete();
+                }
             } else {
                 message.reply("Must specify a person");
-message.delete();
+                message.delete();
             }
         } else {
-            message.reply("Must specify a person");
-message.delete();
-        }
-} else {
-message.reply("Insuffiecient Permissions");
+            message.reply("Insuffiecient Permissions");
 
+        }
     }
-}
 }
--- commands/kick.js
+++ commands/kick.js
@@ -6,43 +6,43 @@ module.exports = {
     description: 'Kicks a person',
     execute(message, args) {
 
-if(message.author.hasPermissions(['KICK_MEMBERS'])){
+        if (message.member.roles.some(r => ["MessageBot"].includes(r.name))) {
 
-        const user = message.mentions.users.first();
+            const user = message.mentions.users.first();
 
-        if (user) {
-            const member = message.guild.member(user);
+            if (user) {
+                const member = message.guild.member(user);
 
-            if (member) {
-                member.kick(`Kicked by: ${message.author.tag}`).then(() => {
-                    const KickSuccess = new Discord.RichEmbed()
-                    .setTitle("✅ Successful Kick")
-                    .setDescription(`Successfully kicked ${user.tag}`)
-                    .setColor(0x00FF00)
-                    .setTimestamp();
-                    message.channel.send({ embed: KickSuccess});
-message.delete();
+                if (member) {
+                    member.kick(`Kicked by: ${message.author.tag}`).then(() => {
+                        const KickSuccess = new Discord.RichEmbed()
+                            .setTitle("✅ Successful Kick")
+                            .setDescription(`Successfully kicked ${user.tag}`)
+                            .setColor(0x00FF00)
+                            .setTimestamp();
+                        message.channel.send({ embed: KickSuccess });
+                        message.delete();
 
-                }).catch(err => {
-                    const KickFailed = new Discord.RichEmbed()
-                    .setTitle("❌ Failed Kick")
-                    .setDescription(`Unable to kick ${user.tag}`)
-                    .setColor('RED')
-                    .setTimestamp();
-                    message.channel.send({ embed: KickFailed});
-message.delete();
-                    console.log(err);
-                });
+                    }).catch(err => {
+                        const KickFailed = new Discord.RichEmbed()
+                            .setTitle("❌ Failed Kick")
+                            .setDescription(`Unable to kick ${user.tag}`)
+                            .setColor('RED')
+                            .setTimestamp();
+                        message.channel.send({ embed: KickFailed });
+                        message.delete();
+                        console.log(err);
+                    });
+                } else {
+                    message.reply("Must specify a person");
+                    message.delete();
+                }
             } else {
                 message.reply("Must specify a person");
-message.delete();
+                message.delete();
             }
         } else {
-            message.reply("Must specify a person");
-message.delete();
+            message.reply("Insufficient Permissions");
         }
-} else {
-message.reply("Insufficient Permissions");
-}
     }
 }
