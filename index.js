e520a3b42ca55ef0ceaf8cfab62e7704aa2bd0a7
SpinGaming <46069683+SpinGaming@users.noreply.github.com>
2019-12-16 22:38:35 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -42,7 +42,17 @@ bot.on('message', message => {
             break;
 
         case 'help':
-            bot.commands.get('help').execute(message, args);
+            const HelpEmbed = new Discord.RichEmbed()
+        .setTitle("ℹ️ | Bot Help")
+        .addField("Prefix", "$")
+        .addField("$ping", "Displays Bot Ping", true)
+        .addField("$botinfo", "Displays Bot Info", true)
+        .addField("$ban", "Bans a member", true)
+        .addField("$kick", "Kicks a member", true)
+        .setThumbnail(bot.user.displayavatarURL)
+        .setAuthor(bot.user.username, bot.user.displayAvatarURL)
+        .setTimestamp();
+        message.channel.send({ embed: HelpEmbed });
             break;
 
     }
