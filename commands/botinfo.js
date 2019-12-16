3aea2dc869bef5d8cce76b619669a0dc863abdff
SpinGaming <46069683+SpinGaming@users.noreply.github.com>
2019-12-16 22:42:16 +0000

    Add files via upload

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -7,15 +7,31 @@ module.exports = {
     description: 'Displays Bot Info',
     execute(message, args){
 
-        let totalSeconds = (bot.uptime / 1000);
-        let days = Math.floor(totalSeconds / 86400);
-        let hours = Math.floor(totalSeconds / 3600);
-        totalSeconds %= 3600;
-        let minutes = Math.floor(totalSeconds / 60);
-        let seconds = totalSeconds % 60;
+        function convertMS(ms) {
+            var d, h, m, s;
+            s = Math.floor(ms / 1000);
+            m = Math.floor(s / 60);
+            s = s % 60;
+            h = Math.floor(m / 60);
+            m = m % 60;
+            d = Math.floor(h / 24);
+            h = h % 24;
+            return {
+                d: d
+                , h: h
+                , m: m
+                , s: s
+            };
+        };
     
-        let uptime = `${days} days, ${hours} hours, ${minutes} minutes and ${seconds} seconds`;
-
+        let u = convertMS(bot.uptime);
+        let uptime = u.d + " days : " + u.h + " hours : " + u.m + " minutes : " + u.s + " seconds"
+    
+        const duration = moment.duration(bot.uptime)
+    
+        let member = message.guild.member
+        user = member.user;
+        
         let embed = new Discord.RichEmbed()
             .setTitle("Message Bot")
             .setThumbnail("http://icons.iconarchive.com/icons/martz90/hex/512/messages-icon.png")
