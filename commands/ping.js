9372408b787fd2fd8101d458dd728d137d0838d2
SpinGaming <46069683+SpinGaming@users.noreply.github.com>
2019-12-16 16:42:26 +0000

    Add files via upload

--- commands/ping.js
+++ commands/ping.js
@@ -2,24 +2,24 @@ const Discord = require('discord.js');
 module.exports = {
     name: 'ping',
     description: 'Says pong!',
-    execute(message, args){
-        
+    execute(message, args) {
+
         startTime = Date.now();
-    message.delete();
-    message.channel.send("Checking. . .").then((message) => {
-        endTime = Date.now();
+        message.delete();
+        message.channel.send("Checking. . .").then((message) => {
+            endTime = Date.now();
 
-        let ping = Math.round(endTime - startTime)
-        let rounded = ping / 1000
-        let member = message.author
+            let ping = Math.round(endTime - startTime)
+            let rounded = ping / 1000
+            let member = message.author
 
-        let embed = new Discord.RichEmbed()
-        .setTitle('🏓 Pong!')
-        .setColor(0x00FF00)
-        .setDescription(`Current ping: **${ping}ms**`)
-        .setTimestamp();
+            let embed = new Discord.RichEmbed()
+                .setTitle('🏓 Pong!')
+                .setColor(0x00FF00)
+                .setDescription(`Current ping: **${ping}ms**`)
+                .setTimestamp();
 
-        message.edit({ embed: embed })
-    });
+            message.edit({ embed: embed })
+        });
     }
 }
