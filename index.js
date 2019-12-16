241dd490beae18d389cffe3e3f3842e09d5f29b0
SpinGaming <46069683+SpinGaming@users.noreply.github.com>
2019-12-16 22:48:05 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -30,7 +30,42 @@ bot.on('message', message => {
             break;
 
         case 'botinfo':
-            bot.commands.get('botinfo').execute(message, args);
+            function convertMS(ms) {
+                var d, h, m, s;
+                s = Math.floor(ms / 1000);
+                m = Math.floor(s / 60);
+                s = s % 60;
+                h = Math.floor(m / 60);
+                m = m % 60;
+                d = Math.floor(h / 24);
+                h = h % 24;
+                return {
+                    d: d
+                    , h: h
+                    , m: m
+                    , s: s
+                };
+            };
+    
+            let u = convertMS(bot.uptime);
+            let uptime = u.d + " days : " + u.h + " hours : " + u.m + " minutes : " + u.s + " seconds"
+    
+            const duration = moment.duration(bot.uptime)
+    
+            let member = message.guild.member
+            user = member.user;
+    
+            let embed = new Discord.RichEmbed()
+                .setAuthor(bot.user.username, bot.user.displayAvatarURL)
+                .setColor(config.color)
+                .addField('Uptime', uptime)
+                .addField('Version', config.version, true)
+                .addField('Library', 'Discord.JS', true)
+                .addField('Developer', '<@264384820698677249>')
+                .setFooter(`ID: ${bot.user.id} | Bot Created`)
+                .setTimestamp(moment.utc(bot.user.createdAt).format('MM.DD.YYYY'));
+    
+            message.channel.send({ embed: embed });
             break;
 
         case 'ban':
