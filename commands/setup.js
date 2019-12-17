e27624e41250e3a32d227855b0e9f6fff6388c3e
SpinGaming <46069683+SpinGaming@users.noreply.github.com>
2019-12-17 23:13:17 +0000

    Add files via upload

---
+++ commands/setup.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+module.exports = {
+    name: 'setup',
+    description: 'Displays Setup Help',
+    execute(message, args) {
+
+            let embed = new Discord.RichEmbed()
+                .setTitle('ℹ️ | Setup Help')
+                .setColor('BLUE')
+                .addField("Kick & Ban Perms", "To give a member kick or ban permission, you must create a role named 'MessageBot' and give that role to the member. The bot will only accept this role as a valid admin.")
+                .setTimestamp();
+
+            message.edit({ embed: embed })
+        
+    }
+}
