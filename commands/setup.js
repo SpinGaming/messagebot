817a46a1e07c3d2b6d8edde220138d503d55a0f0
SpinGaming <46069683+SpinGaming@users.noreply.github.com>
2019-12-17 23:19:46 +0000

    Add files via upload

--- commands/setup.js
+++ commands/setup.js
@@ -10,7 +10,7 @@ module.exports = {
                 .addField("Kick & Ban Perms", "To give a member kick or ban permission, you must create a role named 'MessageBot' and give that role to the member. The bot will only accept this role as a valid admin.")
                 .setTimestamp();
 
-            message.send({ embed: embed })
+            message.channel.send({ embed: embed })
             message.delete();
         
     }
